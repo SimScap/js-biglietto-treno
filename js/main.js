6ae1cd4ebd2447e121f38dda42e7ca4fc7ec4662
SimoneScapin <simonescap87@gmail.com>
2022-02-22 18:51:27 +0100

    un pò di custom

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
-const distanza = parseInt(prompt("Quanti chilometri devi percorrere?") );
+const distanza = parseInt(prompt("Quanti chilometri devi percorrere?"));
+document.getElementById('distance').innerHTML = distanza;
 const eta = parseInt(prompt("Quanti anni hai?"));
+document.getElementById('age').innerHTML = eta;
 const prezzoKm = 0.21;
-const prezzoSenzaSconto = prezzoKm * distanza;
+let prezzoSenzaSconto = prezzoKm * distanza;
 
 if (eta <= 18){
-    document.getElementById('costo').innerHTML = `il prezzo del biglietto è ${(prezzoSenzaSconto / 100) * 80} euro `;   
+    document.getElementById('costo').innerHTML = `Il prezzo del biglietto è di ${((prezzoSenzaSconto / 100) * 80).toFixed(2)} Euro `;   
 }
-else if (eta >=65){
-    document.getElementById('costo').innerHTML = `il prezzo del biglietto è ${(prezzoSenzaSconto / 100) * 60} euro `;   
+else if (eta >= 65){
+    document.getElementById('costo').innerHTML = `Il prezzo del biglietto è di ${((prezzoSenzaSconto / 100) * 60).toFixed(2)} Euro `;   
 }
 else{
-    document.getElementById('costo').innerHTML = `il prezzo del biglietto è ${prezzoSenzaSconto} euro `;   
+    document.getElementById('costo').innerHTML = `Il prezzo del biglietto è di ${prezzoSenzaSconto.toFixed(2)} Euro `;   
 }
+
+
